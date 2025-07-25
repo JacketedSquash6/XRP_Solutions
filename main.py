5a99e21709b2d9741824989f501b7130153a3d1e
Taras Pylypenko <taraspylypenko@gmail.com>
2025-07-25 15:08:38 -0400

    written

--- main.py
+++ main.py
@@ -1,56 +1,84 @@
-from templates import Action, Facing
-from navigator import Navigator
-from simulator_pilot import SimulatorPilot
-from robot_pilot import RobotPilot
 import random
+from XRPLib.defaults import *
 
-def main():
-    height, width = (5,6)
-    map = [[(random.random() < 0.1) for i in range(width)] for j in range(height)]
-    #pilot = SimulatorPilot(map)
-    pilot = RobotPilot()
-    navigator = Navigator((0,0), Facing.NORTH, map)
+BASE_EFFORT = 0.3
+TURN_CONSTANT = 0.5
 
-    targets = []
-    while len(targets) < 10:
-        t_row = random.randint(0, height-1)
-        t_col = random.randint(0, width-1)
-        if not map[t_row][t_col]:
-            targets.append((t_row, t_col))
+height = 6
+width = 6
 
-    #pilot.display()
-    score = 0
-    for t in targets:
-        print("Navigating to Target", t)
-        navigator.set_target(t)
-        action = None
-        for i in range(100): # navigator gets 100 moves to get us to target
-            action = navigator.select_action()
-            if action == Action.DONE:
-                break
-            take_action(pilot, action)
-            #pilot.display()
-        
-        if pilot.test_target(t):
-            print("Arrived at Target", t)
-            score += 1
-        else:
-            print("Failed to arrive at Target", t)
+targets = []
+for i in range(10):
+    t_row = random.randint(0, height-1)
+    t_col = random.randint(0, width-1)
+    targets.append((t_row, t_col))
+
+position = (0, 0)
+
+for target in targets:
+    ### We need to go some number of spaces North or South depending on the difference between ourselves and our target
+
+    vertical = target[0] - position[0]
+    if vertical < 0:
+        # if we need to drive south, turn around
+        turn_left()
+        turn_left()
+    # the absolute value tells you how many intersections to go forward
+    for i in range(abs(vertical)):
+        forward()
+    if vertical < 0:
+        # we are facing south, and the following code will assume that we're facing North, so let's turn back North.
+        # if you're clever, you can avoid this extra turning, but you have to be careful to keep track of which way you're facing
+        turn_left()
+        turn_left()
     
-    print("The Robot successfully navigated to", score, "out of", len(targets), "targets")
+    ### Now we're on the right row. Let's get to the right column
 
+    horizontal = target[1] - position[1]
+    # turn left or right depending on which way we need to go
+    if horizontal < 0:
+        turn_left()
+    else: 
+        turn_right()
+    
+    # travel forward
+    for i in range(abs(horizontal)):
+        forward()
+    
+    # turn back to face North for the next cycle
+    if horizontal < 0:
+        turn_right()
+    else: 
+        turn_left()
 
         
-def take_action(pilot, action):
-    if action == Action.TURN_LEFT:
-        return pilot.turn_left()
-    if action == Action.TURN_RIGHT:
-        return pilot.turn_right()
-    if action == Action.FORWARD:
-        return pilot.forward()
-    else:
-        return True
 
+def turn_left(self):
+    drivetrain.turn(80)
+    while reflectance.get_right()<0.8:
+            drivetrain.set_effort(-BASE_EFFORT, BASE_EFFORT)
+            
+    return True
 
-if __name__ == "__main__":
-    main()
+def turn_right(self): 
+    drivetrain.turn(-80)
+    while reflectance.get_left()<0.8:
+            drivetrain.set_effort(BASE_EFFORT, -BASE_EFFORT)
+
+    return True
+
+def forward(self):
+    while True:
+        self.line_follow()
+        if self.check_intersection():
+            drivetrain.straight(10)
+            break
+
+    return True
+
+def line_follow(self):
+    turn_offset = TURN_CONSTANT * (reflectance.get_left() - reflectance.get_right())
+    drivetrain.set_effort(BASE_EFFORT - turn_offset, BASE_EFFORT + turn_offset)
+    
+def check_intersection(self):
+    return (reflectance.get_left()>0.81 and reflectance.get_right()>0.81)
