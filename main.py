cf4b675c807668694d7616dec8070a1e083e78ab
Taras Pylypenko <taraspylypenko@gmail.com>
2025-07-27 14:14:06 -0400

    commit

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from XRPLib.defaults import *
 
-### 1.
-# while True:
-#     print(reflectance.get_left(), reflectance.get_right())
-
-
-### 2.
 BASE_SPEED = 0.25
 GAIN = 0.3
 
@@ -15,5 +9,15 @@ def line_follow(self):
     turn_offset = GAIN * (left_reflectance - right_reflectance)
     drivetrain.set_effort(BASE_SPEED - turn_offset, BASE_SPEED + turn_offset)
 
+def check_intersection(self):
+    return (reflectance.get_left()>0.81 and reflectance.get_right()>0.81)
+
+def turn_around(self):
+    drivetrain.turn(175)
+    while reflectance.get_right()<0.8:
+            drivetrain.set_effort(-BASE_SPEED, BASE_SPEED)
+
 while True:
-    line_follow()
+    self.line_follow()
+    if self.check_intersection():
+        turn_around()
