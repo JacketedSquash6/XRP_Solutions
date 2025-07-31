c8aaf715851984c8b74702a2ffd0441a22e76082
Taras Pylypenko <taraspylypenko@gmail.com>
2025-07-31 15:46:01 -0400

    robot_pilot functionality

--- robot_pilot.py
+++ robot_pilot.py
@@ -1,35 +1,69 @@
 from XRPLib.defaults import *
+from utilities import NORTH, EAST, SOUTH, WEST, facing_left, facing_right, TURN_LEFT, TURN_RIGHT, FORWARD
 
 BASE_EFFORT = 0.3
 TURN_CONSTANT = 0.5
 
-class RobotPilot(Pilot):
+class RobotPilot:
+    def __init__(self):
+        self.robot_row = 0
+        self.robot_col = 0
+        self.robot_facing = NORTH
+
     def turn_left(self):
         drivetrain.turn(80)
         while reflectance.get_right()<0.8:
                drivetrain.set_effort(-0.3,0.3)
-               
+        self.robot_facing = facing_left(self.robot_facing)
         return True
     
     def turn_right(self): 
         drivetrain.turn(-80)
         while reflectance.get_left()<0.8:
                drivetrain.set_effort(0.3,-0.3)
-
+        self.robot_facing = facing_right(self.robot_facing)
         return True
     
     def forward(self):
+        if False: #TODO distance sensor
+            return False
         while True:
             self.line_follow()
             if self.check_intersection():
                 drivetrain.straight(10)
                 break
+        
+        if self.robot_facing == NORTH:
+            self.robot_row = self.robot_row + 1
+        elif self.robot_facing == EAST:
+            self.robot_col = self.robot_col + 1
+        elif self.robot_facing == SOUTH:
+            self.robot_row = self.robot_row - 1
+        elif self.robot_facing == WEST:
+            self.robot_col = self.robot_col - 1
+        
+        return True
+    
+    def get_position(self):
+        return (self.robot_row, self.robot_col)
+    def get_facing(self):
+        return self.robot_facing
+    def do_actions(self, action_list):
+        for action in action_list:
+            if action == TURN_LEFT:
+                result = self.turn_left()
+            elif action == TURN_RIGHT:
+                result = self.turn_right()
+            elif action == FORWARD:
+                result = self.forward()
+            else:
+                raise RuntimeError("Robot wants to do an undefined action")
+            if result == False:
+                return False
 
         return True
         
-    def test_target(self, target):
-        return False
-        
+    # Helper Functions
     def line_follow(self):
         turn_offset = TURN_CONSTANT * (reflectance.get_left() - reflectance.get_right())
         drivetrain.set_effort(BASE_EFFORT - turn_offset, BASE_EFFORT + turn_offset)
