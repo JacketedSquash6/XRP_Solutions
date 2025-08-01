09dcf7e55e0ebdfe3c01ae09b90b9956d3f23591
Taras Pylypenko <taraspylypenko@gmail.com>
2025-08-01 18:04:01 -0400

    commit

--- robot_pilot.py
+++ robot_pilot.py
@@ -1,26 +1,19 @@
 from XRPLib.defaults import *
-from utilities import NORTH, EAST, SOUTH, WEST, facing_left, facing_right, TURN_LEFT, TURN_RIGHT, FORWARD
+from utilities import TURN_LEFT, TURN_RIGHT, FORWARD
 
 BASE_EFFORT = 0.3
 TURN_CONSTANT = 0.5
 
 class RobotPilot:
-    def __init__(self):
-        self.robot_row = 0
-        self.robot_col = 0
-        self.robot_facing = NORTH
-
     def turn_left(self):
         drivetrain.turn(80)
         while reflectance.get_right()<0.8:
                drivetrain.set_effort(-0.3,0.3)
-        self.robot_facing = facing_left(self.robot_facing)
     
     def turn_right(self): 
         drivetrain.turn(-80)
         while reflectance.get_left()<0.8:
                drivetrain.set_effort(0.3,-0.3)
-        self.robot_facing = facing_right(self.robot_facing)
     
     def forward(self):
         while True:
@@ -28,15 +21,6 @@ class RobotPilot:
             if self.check_intersection():
                 drivetrain.straight(10)
                 break
-        
-        if self.robot_facing == NORTH:
-            self.robot_row = self.robot_row + 1
-        elif self.robot_facing == EAST:
-            self.robot_col = self.robot_col + 1
-        elif self.robot_facing == SOUTH:
-            self.robot_row = self.robot_row - 1
-        elif self.robot_facing == WEST:
-            self.robot_col = self.robot_col - 1
     
     def get_position(self):
         return (self.robot_row, self.robot_col)
